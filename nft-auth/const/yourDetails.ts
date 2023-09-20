dc67e23d13dc28cc9a96d6209163f57199d85c97
Sigit Haryoyudanto <hello@sigit.co.uk>
2023-09-20 10:06:44 +0000

    first commit

---
+++ nft-auth/const/yourDetails.ts
@@ -0,0 +1,11 @@
+// Replace this with your contract address
+export const contractAddress = "0xf82F9B02B6Ea144d1E08944dbbdECc610d54d343";
+
+// Replace this with your domain name.
+export const domainName = "gitpod.io";
+
+// Minimum number of tokens required to access
+export const minimumBalance = 1;
+
+// Token ID for ERC-1155 tokens
+export const erc1155TokenId = 0;
---
+++ nft-auth/src/pages/_app.tsx
@@ -0,0 +1,33 @@
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Sepolia } from "@thirdweb-dev/chains";
+import Head from "next/head";
+import { domainName } from "../../const/yourDetails";
+import "../styles/globals.css";
+
+// This is the chain your dApp will work on.
+const activeChain = "Sepolia";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebProvider
+      activeChain={Sepolia}
+      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+      authConfig={{
+        domain: domainName,
+        authUrl: "/api/auth",
+      }}
+    >
+      <Head>
+        <title>NFT Gated Website</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Learn how to use the thirdweb Auth SDK to create an NFT Gated Website"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
